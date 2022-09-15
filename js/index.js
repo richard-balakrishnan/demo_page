89adc5500f057bb297ea09a451b9a2ee9f6c0d00
richard-balakrishnan <richard.balakrishnan@syncfusion.com>
2022-09-15 09:10:18 +0530

    clean up done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var toggler = document.getElementsByClassName('navbar-toggler')[0];
 var nav = document.getElementsByClassName('navbar');
+var mybutton = document.getElementById("backToTop");
 
 toggler.addEventListener('click', function () {
     var toggler_dataset = this.dataset['bsTarget'].replace('#', '');
@@ -7,7 +8,7 @@ toggler.addEventListener('click', function () {
     sideToggleElement.classList.toggle("open");
 });
 
-window.onscroll = function () { navHighlightEnableFunction(); };
+window.onscroll = function () { scrollToTopEnableFunction(); navHighlightEnableFunction(); };
 
 
 function navHighlightEnableFunction() {
@@ -18,5 +19,13 @@ function navHighlightEnableFunction() {
     else {
         nav[0].style.backgroundColor = "#fff";
         nav[0].style.boxShadow = "0 8px 11px -6px rgba(0, 0, 0, 0.12)";
+    }
+}
+
+function scrollToTopEnableFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
 }
